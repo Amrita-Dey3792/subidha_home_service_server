1030aa7e25b341d2860e5206711ec9c5bfee46f0
amrita3792 <amritadey701@gmail.com>
2024-01-02 22:52:41 +0600

    added search functionality

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 // middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
@@ -19,83 +29,139 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
-  const allServicesCollection = client.db('SubidhaHomeService').collection('services');
+  const allServicesCollection = client
+    .db("SubidhaHomeService")
+    .collection("services");
+  const usersCollection = client.db("SubidhaHomeService").collection("users");
+
   try {
-    app.get('/allServiceCategories', async (req, res) => {
+    app.get("/allServiceCategories", async (req, res) => {
       try {
         const query = {};
-        const serviceCategories = await allServicesCollection.find(query).toArray();
-        const allServiceCategory = serviceCategories.map(serviceCategory => ({
+        const serviceCategories = await allServicesCollection
+          .find(query)
+          .sort({ _id: 1 })
+          .toArray();
+        const allServiceCategory = serviceCategories.map((serviceCategory) => ({
           _id: serviceCategory._id,
           serviceName: serviceCategory.serviceName,
           icon: serviceCategory.icon,
-          totalService: serviceCategory.subCategories.length
+          totalService: serviceCategory.subCategories.length,
         }));
-        console.log(allServiceCategory.length);
-        console.log(allServiceCategory);
         res.send(allServiceCategory);
       } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
       }
     });
 
-    app.get('/allServiceCategories/:id', async(req, res) => {
-        try {
-          const serviceId = req.params.id;
-          console.log(serviceId)
-          const query = {
-            _id: new ObjectId(serviceId),
-          }
-          const service = await allServicesCollection.findOne(query);
-          res.send(service);
-        } catch(error) {
-          console.log(error);
-          res.status(500).send('Internal Server Error');
-        }
-
+    app.get("/allServiceCategories/:id", async (req, res) => {
+      try {
+        const serviceId = req.params.id;
+        console.log(serviceId);
+        const query = {
+          _id: new ObjectId(serviceId),
+        };
+        const service = await allServicesCollection.findOne(query);
+        res.send(service);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+      }
     });
 
-    app.get('/subcategory/:categoryId/:subCategoryId', async (req, res) => {
+    app.get("/subcategory/:categoryId/:subCategoryId", async (req, res) => {
       try {
         const categoryId = req.params.categoryId;
         const subCategoryId = req.params.subCategoryId;
 
         console.log(categoryId);
         console.log(subCategoryId);
-        
+
         const query = {
           _id: new ObjectId(categoryId),
-        }
+        };
         const serviceCategory = await allServicesCollection.findOne(query);
-    
+
         if (!serviceCategory) {
-          return res.status(404).send('Service category not found');
+          return res.status(404).send("Service category not found");
         }
-        
-        const subCategory = serviceCategory.subCategories.find(subCategory => subCategory.id === subCategoryId);
-    
+
+        const subCategory = serviceCategory.subCategories.find(
+          (subCategory) => subCategory.id === subCategoryId
+        );
+
         if (!subCategory) {
-          return res.status(404).send('Subcategory not found');
+          return res.status(404).send("Subcategory not found");
         }
-    
+
         res.send({
           subCategory,
           serviceOverview: serviceCategory.serviceOverview,
-          faq: serviceCategory.faq
+          faq: serviceCategory.faq,
         });
       } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
       }
     });
-    
-    
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = {
+        uid: user.uid,
+      };
+      let result = await usersCollection.findOne(query);
+
+      if (result?.uid === user.uid) {
+        const filter = { uid: user.uid };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            lastLogin: user.lastLogin,
+          },
+        };
+        result = await usersCollection.updateOne(filter, updateDoc, options);
+        console.log(result);
+        res.send({ acknowledged: true });
+        return;
+      }
+      result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const searchTerm = req.query.searchText;
+      // console.log(searchTerm)
+      try {
+        if (searchTerm) {
+          const users = await usersCollection.find().toArray();
+          const filteredUsers = users?.filter(user => {
+            console.log(user?.userName);
+            console.log(searchTerm)
+     
+              // console.log(user.phone?.toLowerCase().includes(searchTerm.toLowerCase()) > -1)
+              return user.userName?.toLowerCase().search(searchTerm.toLowerCase()) > -1 || user.email?.toLowerCase().search(searchTerm.toLowerCase()) > -1 || user.phone?.toLowerCase().search(searchTerm.toLowerCase()) > -1
+          })
+          console.log(filteredUsers)
+          res.send(filteredUsers);
+          return;
+        }
+        const users = await usersCollection.find().toArray();
+        res.json(users);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -103,12 +169,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Subidha Home Service Server is Running...');
+app.get("/", (req, res) => {
+  res.send("Subidha Home Service Server is Running...");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Home Services Server app Listening on Port: ${port}`);
 });
-
